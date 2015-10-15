add9e5d658e8e63fbeb8f956978284df67575f3d
blahblahkams@yahoo.com <blahblahkams@yahoo.com>
2015-10-15 14:35:18 -0400

    filtering function now reformats articles into the feature / non-feature layout

--- filter-3.js
+++ filter-3.js
@@ -1,62 +1,462 @@
 document.addEventListener("DOMContentLoaded", function(event) {
    
     $("#preloader").remove();
-    
-    var all = $(".twelve .row");
-    
+        
     $("nav a").click(function(){
         $(".selected").removeClass("selected");
         this.className += " selected";    
                 
         if (this.id=="all"){
-//            $("a.wp-image-1086").slideDown();
-//            $("a.wp-image-1086").each(function(i, el){
-//                if (alignments[i]){
-//                    $(this).removeClass("alignright").addClass("alignleft");
-//                } else {
-//                    $(this).removeClass("alignleft").addClass("alignright");
-//                }
-//            });
-            
-            $(".article").show();
+            $(".row.filtered").remove();
+            $(".page-body .row").show();
         } else {
-                        
-            $(".article").hide();
-            var selector = ".article."+this.id;
-            $(selector).show();
-
             
-//            all.hide();
-//            $(".filtered").remove();
-//            
-//            $(".page-body").append("<div class='filtered'></div>");
-//            
-//            var selector = ".article."+this.id;
-//            var counter = 0;
-//            var row;
-//            $(selector).slideDown().each(function(i,el){
-//                console.log(counter);
-//                if (counter==0){
-//                    $(".filtered").append( $(this).addClass("feature").wrap("<div class='row'></div>"));
-//                    counter = 1;
-//                } else if (counter==1 || counter==4){
-//                    row = document.createElement("div");
-//                    row.className = "row";
-//                    row.appendChild($(this).removeClass("feature")[0]);
-//                    $(".filtered").append(row);
-//                    counter++;
-//                    
-//                } else {
-//                    row.appendChild($(this).removeClass("feature")[0]);
-//                    $(".filtered").append(row); 
-//                    if (counter==6) counter=0;
-//                }
-//            });
+            $(".page-body .row").hide();
+            $(".row.filtered").remove();
+
+            var tag = this.id;
+            
+            var filtered = [];
+            for (var month in articles){
+                for (var i in articles[month]){
+                    if (articles[month][i].tags.indexOf(tag) > -1){
+                        filtered.push(articles[month][i]);
+                    }
+                }
+            }
+            
+            var row;
+            for (var j=0;j<filtered.length;j++){
+               
+                if (j%7==0 ){
+                    addFeature(filtered[j]);
+                    continue;
+                } 
+                
+                if (j%7==1 || j%7==4){
+                    row = document.createElement("div");
+                    row.className = "row filtered";
+                    $(".page-body").append(row);
+                } 
+                
+                addArticleCard(filtered[j], row);
+                    
+            }
         }
         
     });
 
 });
 
+function addFeature(article){
+    var row = document.createElement("div");
+    row.className = "row filtered";
+    
+    var link = document.createElement("a");
+ 
+    link.href = article.link;
+    link.className = "size-large wp-image-1086 twelve columns article feature ";
+    link.className += article.tags;
+    link.style.backgroundImage = article.img;
+    
+    var caption = document.createElement("span");
+    
+    var title = document.createElement("span");
+    title.textContent = article.title;
+    title.className = "article-title";
+    
+    var subtitle = document.createElement("span");
+    subtitle.textContent = article.subtitle;
+    subtitle.className = "article-subtitle";
 
+    var byline = document.createElement("span");
+    byline.textContent = "By "+article.byline;
+    byline.className = "article-byline";
+    
+    caption.appendChild(title);
+    caption.appendChild(document.createElement("br"));
+    caption.appendChild(subtitle);
+    caption.appendChild(document.createElement("br"));
+    caption.appendChild(byline); 
+    
+    link.appendChild(caption);
+    row.appendChild(link);
+    
+    $(".page-body").append(row);
+}
+
+function addArticleCard(article, row){
+
+    var card = document.createElement("div");
+    card.className = "four columns article ";
+    card.className += article.tags;
+
+    var link = document.createElement("a");
+    link.className = "four columns wp-image-1086 "
+    link.href = article.link;
+    link.style.backgroundImage = article.img;
+    
+    var caption = document.createElement("div");
+    
+    var title = document.createElement("span");
+    title.textContent = article.title;
+    title.className = "article-title";
+    
+    var subtitle = document.createElement("span");
+    subtitle.textContent = article.subtitle;
+    subtitle.className = "article-subtitle";
+
+    var byline = document.createElement("span");
+    byline.textContent = "By "+article.byline;
+    byline.className = "article-byline";
+    
+    caption.appendChild(title);
+    caption.appendChild(document.createElement("br"));
+    caption.appendChild(subtitle);
+    caption.appendChild(document.createElement("br"));
+    caption.appendChild(byline); 
+    
+    card.appendChild(link);
+    card.appendChild(caption);
+    
+    row.appendChild(card);
+}
+
+
+var articles = {
+    
+    "April 2015": 
+    [
+        {
+        "title": "Closing the Achievement Gap",
+        "subtitle": "After a turbulent year, the UNC-system is wondering what's next",
+        "byline": "Liz Crampton",
+        "link": "https://synapse.atavist.com/achievement-gap",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/04/well.jpg)",
+        "tags": "campus education"
+        },
+
+        {
+        "title": "Learning in Two Languages",
+        "subtitle": "An historic Chapel Hill elementary school explores what could represent the future of education in the U.S.",
+        "byline": "Camila Molina",
+        "link": "https://synapse.atavist.com/duallanguage",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~dsc0184-1430106231-56.jpg )",
+        "tags": "community education"
+        },
+
+        {
+        "title": "Home Cooked",
+        "subtitle": "How the story of a restaurant parallels the story of a life",
+        "byline": "Jackson Knapp",
+        "link": "https://synapse.atavist.com/mama-dips",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/04/mamad.jpeg)",
+        "tags": "community"
+        },
+
+        {
+        "title": "Game Face",
+        "subtitle": "Student-athletes learn to care for every part of the body, but the mind is often overlooked",
+        "byline": "Sarah Niss",
+        "link": "https://synapse.atavist.com/game-face",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~img2291-1430063972-100.jpg )",
+        "tags": "campus sports"
+        },
+
+        {
+        "title": "Gum",
+        "subtitle": "How Alzheimer's damages a mind, but not a memory",
+        "byline": "Dylan Howlett",
+        "link": "https://synapse.atavist.com/gum",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/03/HowlettWest_22.jpeg)",
+        "tags": ""
+        }
+    ],
+    
+    "March 2015": 
+    [
+        {
+        "title": "Marathon",
+        "subtitle": "What goes through a runner's mind in 26.2 miles",
+        "byline": "Emily Overcarsh",
+        "link": "https://synapse.atavist.com/marathon",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/10/marathon.jpg)",
+        "tags": ""
+        },
+
+        {
+        "title": "Sister South, Principal",
+        "subtitle": "Navigating the halls of a Southern Catholic school as a non-Catholic student",
+        "byline": "Emily Byrd",
+        "link": "https://synapse.atavist.com/sister_south",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/03/SCAN0052.jpg)",
+        "tags": ""
+        },
+
+        {
+        "title": "Coleslaw",
+        "subtitle": "One writer's journey to the heart of coleslaw wrestling, and how it taught him about where he grew up",
+        "byline": "Rob Harms",
+        "link": "https://synapse.atavist.com/coleslaw_wrestling",
+        "img": "",
+        "tags": ""
+        },
+
+        {
+        "title": "Franklin",
+        "subtitle": "A look at how the landmark has changed over the years",
+        "byline": "Chris Conway",
+        "link": "https://synapse.atavist.com/franklin_then_and_now",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/10/franklin.jpg)",
+        "tags": "multimedia"
+        }
+    ],
+    
+    "February 2015": 
+    [
+        {
+        "title": "An Overlooked History",
+        "subtitle": "African American studies was born from the struggles of black students and staff on campus in the 1960s",
+        "byline": "Jordan Bailey",
+        "link": "https://synapse.atavist.com/aaadhistory",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/BSM5.jpg)",
+        "tags": "campus"
+        },
+
+        {
+        "title": "Guardians of the Forest",
+        "subtitle": "How a tiny bug is destroying millions of acres of our forests",
+        "byline": "Liz Crampton",
+        "link": "https://synapse.atavist.com/guardians",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~hemlockscon-1424191674-89.jpg )",
+        "tags": "science"
+        },
+
+        {
+        "title": "BOG",
+        "subtitle": "",
+        "byline": "Carolyn Bahar",
+        "link": "https://synapse.atavist.com/boardofgovernors",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/BOG_infographic-02.png)",
+        "tags": "education multimedia"
+        },
+
+        {
+        "title": "Rewriting the Code",
+        "subtitle": "How the gender disparity in computer science represents a larger trend in a growing industry",
+        "byline": "Emily Byrd",
+        "link": "https://synapse.atavist.com/rewritingthecode",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/ssIMG_5367.jpg)",
+        "tags": "education science"
+        },
+
+        {
+        "title": "'Bustin' the Envelope'",
+        "subtitle": "How Jay Smith, 65 and still running, defies time and age",
+        "byline": "Rob Harms",
+        "link": "https://synapse.atavist.com/jaysmith",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/sIMG_5353.jpg)",
+        "tags": "community"
+        },
+
+        {
+        "title": "On Being One Community",
+        "subtitle": "",
+        "byline": "Layla Quran",
+        "link": "https://synapse.atavist.com/onecommunity",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/null.jpg)",
+        "tags": ""
+        },
+
+        {
+        "title": "The Story of a Startup",
+        "subtitle": "How UNC has become a launchpad for student innovators",
+        "byline": "Emily Hackeling",
+        "link": "https://synapse.atavist.com/startups",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/DSC_03321.jpg)",
+        "tags": "campus"
+        },
+
+        {
+        "title": "Pushing the Limit",
+        "subtitle": "Some feel slighted by a tuition policy that encourages students to get a degree in eight semesters",
+        "byline": "Sarah Niss",
+        "link": "https://synapse.atavist.com/tuition",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2015/02/WILSONSYNAPSE5.jpg)",
+        "tags": "campus education"
+        },
+
+        {
+        "title": "The Little Church That Could",
+        "subtitle": "In the midst of student living, one Northside church fights to keep a separate, struggling community on its feet",
+        "byline": "Megan Cassella",
+        "link": "https://synapse.atavist.com/church",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/11/1102_littlechurch_conway025.jpg)",
+        "tags": "community"
+        },
+
+        {
+        "title": "College Catch-22",
+        "subtitle": "When it comes to handling students whose behavior is self-threatening, universities are forced to make tough calls",
+        "byline": "Caitlin McCabe",
+        "link": "https://synapse.atavist.com/collegecatch22",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/10/catch22.png)",
+        "tags": "campus science"
+        }
+    ],
+    
+    "December 2014": 
+    [
+        {
+        "title": "RUN GIO 2012",
+        "subtitle": "An oral history of Giovani Bernard's punt return against N.C. State",
+        "byline": "Rob Harms and Dylan Howlett",
+        "link": "https://synapse.atavist.com/gio",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~dsc1907-1418917882-25.jpg )",
+        "tags": "sports"
+        },
+
+        {
+        "title": "Undocumented but Undaunted",
+        "subtitle": "In the college town where they grew up, young Hispanic immigrants come together to find opportunity and community",
+        "byline": "Rachel Herzog",
+        "link": "https://synapse.atavist.com/undocumented",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~img4570cv-1418002908-8.jpg )",
+        "tags": "community"
+        },
+
+        {
+        "title": "The Advance of a Legend",
+        "subtitle": "A chronicle of the shifting fencing world through one man's memory",
+        "byline": "Amy Tsai",
+        "link": "https://synapse.atavist.com/fencing",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~img3745-1417810508-81.jpg )",
+        "tags": "sports"
+        },
+
+        {
+        "title": "In Transition",
+        "subtitle": "For many student veterans, a new battle begins after arriving on campus",
+        "byline": "Sarah Niss",
+        "link": "https://synapse.atavist.com/veterans",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~studentvets-1417918845-20.jpg )",
+        "tags": "campus"
+        }
+    ],
+    
+    "November 2014": 
+    [
+        {
+        "title": "'Distressed, but not in despair'",
+        "subtitle": "After two years of Faith Hedgepeth's murder going unsolved, fresh grief lingers among her family and friends. ",
+        "byline": "Brooke Eller",
+        "link": "https://synapse.atavist.com/hedgepeth",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/11/1004_hedgepeth_crampton010.jpg)",
+        "tags": ""
+        },
+
+        {
+        "title": "Homeless and Need Your Help Thank You",
+        "subtitle": "",
+        "byline": "Rob Harms",
+        "link": "https://synapse.atavist.com/homeless",
+        "img": "",
+        "tags": "community"
+        },
+
+        {
+        "title": "A Basketball Player, the KKK and a Rally No One's Ever Heard Of",
+        "subtitle": "The Strange Tale of Patrick Ewing's Recruitment to UNC",
+        "byline": "Rob Harms",
+        "link": "https://synapse.atavist.com/patrickewing",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~patrickewin-1412367761-2.jpg )",
+        "tags": "sports"
+        },
+
+        {
+        "title": "The Chilling Effect",
+        "subtitle": "Why UNC can't clear one of the last hurdles to campus sustainability",
+        "byline": "Emily Byrd",
+        "link": "https://synapse.atavist.com/chilling-effect",
+        "img": "",
+        "tags": "campus science"
+        },
+
+        {
+        "title": "Teach for North Carolina",
+        "subtitle": "How UNC fuels the TFA pipeline and what that says about education",
+        "byline": "Liz Crampton",
+        "link": "https://synapse.atavist.com/tfa",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/11/1014_TFA_conway001.jpg)",
+        "tags": "education"
+        },
+
+        {
+        "title": "A Homeless Man with a Place to Live",
+        "subtitle": "A story of escaping homelessness but remaining trapped",
+        "byline": "Rob Harms",
+        "link": "https://synapse.atavist.com/jones",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/11/IMG_3253.jpg)",
+        "tags": ""
+        }
+    ],
+    
+    "October 2014": 
+    [
+        {
+        "title": "In the Shadow of the Sealings",
+        "subtitle": "Information related to UNC student Faith Hedgepeth's 2012 murder remains sealed by court order",
+        "byline": "Chelsey Dulaney",
+        "link": "https://synapse.atavist.com/sealings",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~img8926105-1409081113-13.jpg )",
+        "tags": "campus community"
+        },
+
+        {
+        "title": "A Tall Order",
+        "subtitle": "New to the University, Chancellor Carol Folt already has a lengthy to-do list",
+        "byline": "Liz Crampton",
+        "link": "https://synapse.atavist.com/folt",
+        "img": "url(https://synapse.atavist.com/data/files/organization/7429/image/derivative/scale~1200x1200~foltceremon-1412295486-6.jpg )",
+        "tags": "campus education"
+        },
+
+        {
+        "title": "King of a Small Empire",
+        "subtitle": "",
+        "byline": "Claire McNeill",
+        "link": "https://synapse.atavist.com/towing",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/09/IMG_8519.jpg)",
+        "tags": "community"
+        },
+
+        {
+        "title": "The Cost of a Scandal",
+        "subtitle": "",
+        "byline": "Caitlin McCabe",
+        "link": "https://synapse.atavist.com/scandal",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/10/TOC-Scandal.jpg)",
+        "tags": "campus education"
+        },
+
+        {
+        "title": "His Own Hike",
+        "subtitle": "An 18-year-old puts school on hold to find education in a 2,000-mile wilderness.",
+        "byline": "Claire McNeill",
+        "link": "https://synapse.atavist.com/hisownhike",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/10/Massachusetts.jpg)",
+        "tags": "community campus"
+        }, 
+        
+        {
+        "title": "Depression in the Southern Part of Heaven",
+        "subtitle": "",
+        "byline": "Claire McNeill",
+        "link": "https://synapse.atavist.com/depression",
+        "img": "url(http://synapsemag.org/wp-content/uploads/2014/10/quotecrop.jpg)",
+        "tags": "campus science"
+        }
+    ]
+    
+};
                         
